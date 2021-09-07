a48a09bd7743c06d68d2e80fd1dba17f5334b6fa
Sooyoung Kim <sooyounk@andrew.cmu.edu>
2021-09-07 16:34:23 -0400

    Initial Commit

---
+++ Stopwatch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stopwatch
+//
+//  Created by Sooyoung Kim on 07/09/2021.
+//  Copyright © 2021 Sooyoung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
